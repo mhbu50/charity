92086a67c5886d001b415f01bedbef4091375b28
mhbu50 <mhbu50@gmail.com>
2022-01-12 19:50:19 +0300

    chnges

---
+++ charity/charity_management_system/doctype/conditional_review/conditional_review.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, charity and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class ConditionalReview(Document):
+	pass
